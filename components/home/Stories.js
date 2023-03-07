abb10a4be1c2c4bb934a50023bf836ef643c8304
unknown <danielndri1@gmail.com>
2023-03-06 22:42:58 -0500

    added stories bar

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { Users } from '../../data/Users'
+ 
 
 const Stories = () => {
   return (
-    <View>
-      <Text style={{color: 'white'}}>Stories</Text>
+    <View style={{marginBottom: 13}}>
+        <ScrollView  horizontal showsHorizontalScrollIndicator={false}>
+            {Users.map((story, index) => (
+                <View key={index} style={{alignItems: 'center'}}>
+            <Image source={{uri:story.image}}
+            style={styles.story}/>
+            <Text style={{color: 'white'}}>{story.user.length > 11
+             ? story.user.slice(0, 10).toLowerCase() + '...' 
+             : story.user.toLowerCase()}
+             </Text>
+            </View>
+            ))}
+        </ScrollView>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    story: {
+     width: 70,
+     height: 70,
+     borderRadius: 50,
+     marginLeft: 16,
+     borderWidth: 3,
+     borderColor: '#0077b6'
+    },
+  })
+  
 export default Stories
---
+++ data/Users.js
@@ -0,0 +1,23 @@
+export const Users = [
+    {
+    user: 'Your Story',
+    image: 'https://media.istockphoto.com/id/1286810719/photo/smiling-cheerful-young-adult-african-american-ethnicity-man-looking-at-camera-standing-at.jpg?b=1&s=170667a&w=0&k=20&c=w8F2TQzgeu2j-SmDWLHuAyCjFbQJ0smF-RxXcXChTLg='
+},
+{
+    user: 'Lebron James',
+    image: 'https://hot-town-images.s3.amazonaws.com/kwtv/production/2021/December/30/lebron-james-dec-30-2021.1640882757719.jpeg'
+},
+{
+    user: 'Alyce Westbrook',
+    image: 'https://www.bethesdaheadshots.com/wp-content/uploads/2022/05/pretty-black-female-business-headshot.jpg'
+},
+{
+    user: 'Mark Philips',
+    image: 'https://static.wikia.nocookie.net/deathbattle/images/b/bb/Vamarkphil.jpg/revision/latest?cb=20210623181315'
+},
+{
+    user: 'Susan Lee',
+    image: 'https://portpholio.com.au/wp-content/uploads/2016/07/IMG_2531.jpg'
+},
+
+]
