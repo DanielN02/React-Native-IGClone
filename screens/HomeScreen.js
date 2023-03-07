7c4de27ffd636ff9cfd26987d344b14d862bb118
unknown <danielndri1@gmail.com>
2023-03-06 21:56:13 -0500

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import Header from '../components/home/Header'
+import Stories from '../components/home/Stories'
+
+
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        {/* <Text style={{color: 'white'}}>Hey</Text> */}
+       <Header />
+       <Stories />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'black',
+        flex: 1,
+    }
+})
+
+export default HomeScreen 
